b721fb7d0ca2c41483965c7846c8358d704defb2
Kostya <kostya2283221337@yandex.ru>
2024-09-14 13:46:39 +0300

    Реализован "Hello World!"

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-
     @GetMapping("/index")
     public String getIndex() {
         return "Hello World!";
