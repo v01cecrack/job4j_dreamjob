e9a570e1fe534f5b7e5a3c6987ca6892662b45a4
Kostya <kostya2283221337@yandex.ru>
2024-09-14 19:12:14 +0300

    Статические ресурсы: CSS, JS [#505079]

---
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -0,0 +1,54 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+
+public class Vacancy {
+
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime creationDate;
+
+    public Vacancy(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+}
